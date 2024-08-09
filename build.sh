04bac1aa0192e42842d03a4730cae882eee5b0b1
FPG Schiba <craftzockerlp@gmail.com>
2024-08-09 20:00:42 +0200

    Updated build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ for os in ${supportedOS[@]}; do
   for arch in ${supportedArches[@]}; do
     extionsion=""
     if [ $os == 'windows' ]; then extionsion='.exe'; fi
-    env GOOS="$os" GOARCH="$arch" go build -o "rusty-shiba-db-$os-$arch$extionsion" rsdb/src
+    env GOOS="$os" GOARCH="$arch" go build -o "./rusty-shiba-db-$os-$arch$extionsion" rsdb/src
   done
 done
